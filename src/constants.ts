eb7db25981e2bb95c75197078dffa72df6a7075f
AKILENG ISAAC <isaacakileng56@gmail.com>
2025-05-26 16:58:19 +0300

    Refresh billable items after editing (#9)

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
 import { restBaseUrl } from '@openmrs/esm-framework';
-
+import { mutate } from 'swr';
 export const apiBasePath = `${restBaseUrl}/billing/`;
 
 export const omrsDateFormat = 'YYYY-MM-DDTHH:mm:ss.SSSZZ';
+
+export const handleMutate = (url: string) => {
+  mutate((key) => typeof key === 'string' && key.startsWith(url), undefined, {
+    revalidate: true,
+  });
+};
