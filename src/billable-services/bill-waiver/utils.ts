949de5f82307954d0565606d77a717a094820b12
Daphne210 <dapnephinerry2@gmail.com>
2025-08-29 15:37:04 +0300

    Changing payment status to pending

--- src/billable-services/bill-waiver/utils.ts
+++ src/billable-services/bill-waiver/utils.ts
@@ -21,7 +21,7 @@ export const createBillWaiverPayload = (
   const processedLineItems = lineItems.map((lineItem) => ({
     ...lineItem,
     billableService: findBillableServiceUuid(billableLineItems, lineItem),
-    paymentStatus: 'PAID',
+    paymentStatus: 'PENDING',
   }));
 
   const processedPayment = {
